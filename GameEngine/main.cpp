27ec08ce30f8acad0f042b4a6aaa96a62eb7c960
Michał Padula <michpad368@student.polsl.pl>
2018-04-15 12:55:02 +0200

    Added usage of SimpleRenderer2D and Renderable2D

--- GameEngine/main.cpp
+++ GameEngine/main.cpp
@@ -1,15 +1,14 @@
 #include <iostream>
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
-#include <glm/gtc/type_ptr.inl>
+#include <glm/gtc/type_ptr.hpp>
 #include <GL/glew.h>
 
 #include "window.h"
 #include "file_reader.h"
 #include "shader.h"
-#include "buffer.h"
-#include "vertexarray.h"
-#include "indexbuffer.h"
+#include "renderable2d.h"
+#include "simple_renderer2d.h"
 
 int main()
 {
@@ -17,63 +16,21 @@ int main()
 	Window window("Window", 800, 600);
 	glClearColor(0.7f, 0.1f, 0.1f, 1.0f);
 
-	/*GLfloat vertices[] =
-	{
-		-0.5f, -0.5f, 0.0f,
-		-0.5f,  0.5f, 0.0f,
-		 0.5f,	0.5f, 0.0f,
-		 0.5f,	0.5f, 0.0f,
-		 0.5f, -0.5f, 0.0f,
-		-0.5f, -0.5f, 0.0f,
-	};
-
-	GLuint vbo;
-	glGenBuffers(1, &vbo);
-	glBindBuffer(GL_ARRAY_BUFFER, vbo);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
-	glEnableVertexAttribArray(0);
-
-	glm::mat4 ortho = glm::ortho(0.0f, 16.0f, 0.0f, 9.0f, -1.0f, 1.0f);
-
-	Shader shader("basic.vert", "basic.frag");
-	shader.enable();
-	glUniformMatrix4fv(glGetUniformLocation(shader.getID(), "projection"), 1, GL_FALSE, glm::value_ptr(ortho));*/
-
-	GLfloat vertices[] =
-	{
-		-0.5f, -0.5f, 0.0f,
-		-0.5f,  0.5f, 0.0f,
-		0.5f,	0.5f, 0.0f,
-		0.5f, -0.5f, 0.0f,
-	};
-
-	GLushort indices[] =
-	{
-		0, 1, 2, 2, 3, 0
-	};
-
-	Buffer* vbo = new Buffer(vertices, 4 * 3, 3);
-	IndexBuffer ibo(indices, 6);
-
-	VertexArray vao;
-	vao.addBuffer(vbo, 0);
-
 	Shader shader("basic.vert", "basic.frag");
 	shader.enable();
 
-	glm::mat4 ortho = glm::ortho(0.0f, 16.0f, 0.0f, 9.0f, -1.0f, 1.0f);
+	glm::mat4 ortho = glm::ortho(0.f, 800.f, 0.f, 600.f, -1.0f, 1.0f);
 	glUniformMatrix4fv(glGetUniformLocation(shader.getID(), "projection"), 1, GL_FALSE, glm::value_ptr(ortho));
+
+	const Renderable2D sprite(glm::vec3(300, 300, 0), glm::vec2(200, 200), glm::vec4(1.000, 0.388, 0.278, 1), shader);
+	SimpleRenderer2D renderer;
 
 	//game loop
 	while (!window.isClosed())
 	{
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-		vao.bind();
-		ibo.bind();
-		glDrawElements(GL_TRIANGLES, ibo.getCount(), GL_UNSIGNED_SHORT, 0);
-		ibo.unbind();
-		vao.unbind();
+		renderer.submit(sprite);
+		renderer.flush();
 		//update window state
 		window.update();
 	}
